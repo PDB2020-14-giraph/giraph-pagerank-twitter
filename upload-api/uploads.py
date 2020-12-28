2451967e20febe9795e96550676b567f40d33940
Sandi Bhirama <43607241+sanbhir14@users.noreply.github.com>
2020-12-28 13:02:50 +0700

    uploaders-kani

---
+++ upload-api/uploads.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
+from app import app
+
+
+@app.route('/uploads')
+def upload_files():
+   return render_template('upload.html')
+	
+@app.route('/uploaders', methods = ['GET', 'POST'])
+def uploads_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
+		
+if __name__ == '__main__':
+   app.run(debug = True)

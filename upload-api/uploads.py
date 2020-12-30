54e5d72f51cf01d1709058542068794de5bf6154
sandi.bhirama <sbhirama@gmail.com>
2020-12-30 19:47:20 +0700

    dev-sandi

--- upload-api/uploads.py
+++ upload-api/uploads.py
@@ -1,18 +1,25 @@
 from flask import Flask, render_template, request
-from werkzeug import secure_filename
+from werkzeug.utils import secure_filename
 from app import app
 
 
 @app.route('/uploads')
 def upload_files():
-   return render_template('upload.html')
+   return render_template('uploads.html')
 	
 @app.route('/uploaders', methods = ['GET', 'POST'])
 def uploads_file():
    if request.method == 'POST':
       f = request.files['file']
       f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+      return render_template('upload-finish.html')
+   
+@app.route('/results')
+def get_results():
+   with open("output-twt.txt", "r") as f: 
+      content = f.read()
+      print(content)
+      return  render_template("results.html", content=content) 
 		
 if __name__ == '__main__':
    app.run(debug = True)
